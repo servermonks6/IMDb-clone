71a3e1314da9aac079e2079ddae058c45e88a7f5
Supratim <supratim@elearnmarkets.com>
2021-05-15 23:13:39 +0530

    poster

--- src/screens/MovieDetails.tsx
+++ src/screens/MovieDetails.tsx
@@ -1,31 +1,69 @@
 import React, {FC, useCallback, useEffect, useState} from 'react';
 import {
+  Dimensions,
   Pressable,
   SafeAreaView,
   StyleSheet,
-  Text,
   TextInput,
   View,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import { getmovieDetails } from '../utils/api';
+import {getmovieDetails} from '../utils/api';
+import {Header, Text, Divider} from 'react-native-elements';
+import {Image} from 'react-native-elements/dist/image/Image';
+
+const Devicewidth = Dimensions.get('window').width;
+const Deviceheight = Dimensions.get('window').height;
 
 const MovieDetails: FC = ({route, navigation}: any) => {
   const {id} = route.params;
-  console.log({id});
+  const [data, setData] = useState<any>([]);
 
   useEffect(() => {
     getmovieDetails(id).then(response => {
       console.log(JSON.stringify(response.data));
 
-      // setData(response.data);
+      setData(response.data);
     });
   }, []);
-  
+
   return (
     <SafeAreaView style={styles.container}>
-      <LinearGradient colors={['#f0f0f0', '#f2c2c6']} style={{flex: 1}}>
-        <Text>Details</Text>
+      <Header
+        leftComponent={{
+          icon: 'arrow-back',
+          color: '#fff',
+          onPress: navigation.goBack,
+        }}
+        centerComponent={{
+          text: 'Movie DEtails',
+          style: {color: '#fff', fontSize: 18},
+        }}
+      />
+      <LinearGradient colors={['#f0f0f0', '#f2c2c6']} style={{height: '100%'}}>
+        <View style={{alignItems:"center",marginTop:25}}>
+          <Image
+            source={{uri: data?.poster}}
+            style={{
+              height: Deviceheight / 2.5,
+              width: Deviceheight / 3,
+              marginHorizontal: 25,
+            }}
+          />
+        </View>
+
+        <Text style={styles.title}>{data.title}</Text>
+        
+        {/* <View style={{flexDirection: 'row'}}>
+          <Text style={styles.text}>height:</Text>
+          <Text style={styles.text}>{data.height}</Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.text}>weight:</Text>
+          <Text style={styles.text}>{data.weight}</Text>
+        </View> */}
+
+        <Divider style={styles.divider} />
       </LinearGradient>
     </SafeAreaView>
   );
@@ -35,6 +73,29 @@ export default MovieDetails;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    backgroundColor: '#E3EBF2',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    marginVertical: 15,
+  },
+  text: {
+    fontSize: 18,
+    margin: 5,
+    fontWeight: '700',
+  },
+  title2: {
+    fontSize: 25,
+    marginVertical: 10,
+    fontWeight: '700',
+  },
+  divider: {
+    backgroundColor: '#b2b2b2',
+    height: 1,
+    width: Devicewidth,
+    marginVertical: 5,
   },
 });
